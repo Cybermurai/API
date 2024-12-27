5606dbcc78248153280069256b0f0b3fd876d6f3
Bartosz Szewczyk <bszewczyk@intype.pl>
2024-12-27 03:25:09 +0100

    Ups

---
+++ config/config.js
@@ -0,0 +1,12 @@
+var config = {
+  host: "127.0.0.1:8100",
+  db: {
+    host: "***",
+    port: "27017",
+    username: "***",
+    password: "***",
+    database: "***111",
+  },
+};
+
+module.exports = { config };
