cbedb638c0848809861e5771ab525c9b2b103282
Bartosz Szewczyk <bszewczyk@intype.pl>
2024-12-27 03:34:40 +0100

    Config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 var config = {
-  host: "127.0.0.1:8100",
+  host: "localhost:8100",
   db: {
-    host: "***",
+    host: "195.167.159.167",
     port: "27017",
-    username: "***",
-    password: "***",
-    database: "***111",
+    username: "mo1449_depo",
+    password: "O2ya8abd",
+    database: "mo1449_depo",
   },
 };
 
