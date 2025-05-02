a1179b6ad6bb0ef50fb77c84fa0747465095a744
Bartosz Szewczyk <bszewczyk@intype.pl>
2025-05-02 09:02:27 +0200

    Fixed

---
+++ config/config.js
@@ -0,0 +1,12 @@
+var config = {
+  host: "localhost:8100",
+  db: {
+    host: "195.167.159.167",
+    port: "27017",
+    username: "mo1449_literpeg",
+    password: "O2ya8abd",
+    database: "mo1449_literpeg",
+  },
+};
+
+module.exports = { config };
